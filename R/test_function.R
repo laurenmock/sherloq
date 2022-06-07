e3c15daa8e6a299dec222a8593ad261a2f2dcb80
Lauren Mock <lmock@natera.local>
2022-06-07 15:32:54 -0400

    added documentation

--- R/test_function.R
+++ R/test_function.R
@@ -1,14 +1,20 @@
-# double input value
-#' Title
+#' Double input value
 #'
-#' @param x
+#' @param x Any value
 #'
-#' @return
-#' @export
+#' @return Returns double the input value.
 #'
 #' @examples
+#' val = 3
+#' test_function(val)
+#' @export
+#'
+#' @importFrom stats rnorm
+
 test_function <- function(x)
   {
     y <- x*2
     return(y)
   }
+
+# use devtools::check()
