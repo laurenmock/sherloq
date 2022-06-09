dffa49ed3c6416292b3cd165f3bb17c659f1361d
Lauren Mock <lmock@natera.local>
2022-06-09 10:38:28 -0400

    finished first draft of LoB function, started LoD function

--- R/LoB.R
+++ R/LoB.R
@@ -1,37 +1,30 @@
 
-#' LoB Calculation
+#' Limit of Blank (LoB) Calculation
 #'
-#' @param df
-#' @param alpha
-#' @param beta
-#' @param nonpar
+#' @param df A data frame
+#' @param alpha Alpha (type I error)
+#' @param parametric Parametric (TRUE) or non-parametric (FALSE). Default is non-parametric.
 #'
-#' @importFrom dplyr
+#' @importFrom dplyr %>%
+#' @importFrom stats qnorm
+#' @importFrom stats sd
 #'
-#' @return
 #' @export
-#'
-#' @examples
-#'
-
-# see page 15
 
 # if 2 or 3 reagent lots --> do these calculations separately for each lot
 # if 4+ reagent lots --> combine all lots
 # apply this function to each reagent lot separately (if 2 or 3) or all the data (if 4+)
 
-LoB <- function(df, alpha=0.05, parametric = c(FALSE, TRUE)){
+LoB <- function(df, alpha=0.05, parametric = FALSE){
 
-  #parametric <- match.arg(parametric)
+  # percentile
+  pct <- 1 - alpha
+
+  # number of reagents
+  B <- nrow(df)
 
   # non-parametric
-  #if(parametric == FALSE){
-
-    # percentile
-    pct <- 1 - alpha
-
-    # number of reagents
-    B <- nrow(df)
+  if(!parametric){
 
     # exact rank position and nearest integers
     rank_exact <- .5 + B*pct
@@ -42,16 +35,26 @@ LoB <- function(df, alpha=0.05, parametric = c(FALSE, TRUE)){
     sorted <- sort(df$pg.ml)
 
     # interpolate measurement for exact rank position (LoB)
-    LoB_val <- sorted[rank_below] + (rank_exact - rank_below)*(sorted[rank_above] - sorted[rank_below])
+    LoB_val <-
+      sorted[rank_below] + (rank_exact - rank_below)*(sorted[rank_above] - sorted[rank_below])
 
-  #}
+  }
 
   # parametric
-  # else{
-  #
-  #   LoB_val <- 1
-  #
-  # }
+  if(parametric){
+
+    # mean and SD
+    mean_B <- mean(df$pg.ml)
+    sd_B <- stats::sd(df$pg.ml)
+
+    # critical value
+    K <- unique(df$sample) %>% length
+    cp <- stats::qnorm(pct) / (1 - (1/(4*(B-K)) ))
+
+    # calculate LoB
+    LoB_val <- mean_B + cp*sd_B
+
+  }
 
   return(LoB_val)
 
---
+++ R/LoD.R
@@ -0,0 +1,53 @@
+
+#' Limit of Detection (LoD) Calculation
+#'
+#' @param df data frame for one reagent lot
+#' @param beta type II error
+#' @param LoB limit of blank
+#' @param approach method of LoD calculation
+#'
+#' @importFrom dplyr %>%
+#' @importFrom stats qnorm
+#' @importFrom stats sd
+#'
+#' @export
+
+# is L really supposed to be across all reagent lots?
+
+LoD <- function(df, beta=0.05, LoB,
+                approach = c("classical parametric", "precision profile", "probit")){
+
+  # # percentile
+  # pct <- 1 - alpha
+  #
+  # # number of reagents
+  # B <- nrow(df)
+
+  # classical nonparametric
+  if(approach == "classical parametric"){
+
+    # maybe?
+    # df$pg.ml <- log(df$pg.ml)
+
+    # standard deviation for each sample
+    sd_sample_df <- do.call(data.frame,
+                             aggregate(pg.ml ~ sample, data = df,
+                                       FUN = function(x) c(sd = stats::sd(x), n = length(x))))
+
+    # pooled standard deviation
+    sd_L <- with(sd_sample_df, sqrt(((pg.ml.n - 1) %*% pg.ml.sd^2)/sum(pg.ml.n - 1)))
+
+    # critical value
+    L <- B # double-check this! says across all reagent lots...
+    J <- unique(df$sample) %>% length
+    cp <- stats::qnorm(pct) / (1 - (1/(4*(L-J)) ))
+
+    # calculate LoD
+    LoD_val <- LoB + cp*sd_L
+  }
+
+  if(approach == "precision profile"){
+
+  }
+
+}
