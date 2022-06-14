9e2954746564b3a3a29782e982335b9c27809cd7
Lauren Mock <lmock@natera.local>
2022-06-14 16:38:16 -0400

    committing first round of LoD updates (classical)

--- R/LoB.R
+++ R/LoB.R
@@ -1,18 +1,21 @@
 
 #' Limit of Blank (LoB) Calculation
 #'
-#' @param df A data frame.
-#' @param col_lot Name (in quotes) of the column with reagent lot number. Can also be NULL (no quotes).
+#' @param df A data frame with blank samples.
+#' @param col_lot Name (in quotes) of the column with reagent lot number.
 #' @param col_sample Name (in quotes) of the column with sample number.
 #' @param col_value Name (in quotes) of the column with measurements.
-#' @param alpha Alpha (type I error). Default is 0.05.
 #' @param parametric Parametric (TRUE) or non-parametric (FALSE). Default is non-parametric.
+#' CLSI guidelines indicate that the non-parametric option is preferred in the vast majority of cases.
+#' @param alpha Alpha (type I error). Default is 0.05.
 #' @param always_sep_lots If FALSE, reagent lots are evaluated according to CLSI guidelines
 #' (all lots evaluated separately if 2 or 3 lots, and all lots evaluated together if >3 lots).
 #' If TRUE, all reagent lots are evaluated separately regardless of the number of lots.
 #' Default is FALSE.
 #'
-#' @examples
+#' @return Returns the limit of blank (LoB) value(s).
+#'
+#' @example
 #' reagent_lot <- c(rep(1, 12*5), rep(2, 12*5))
 #' day <- rep(rep(c(1, 2, 3), each = 4, times = 10))
 #' sample <- rep(c(1, 2, 3, 4, 5), each = 12, times = 2)
@@ -36,54 +39,61 @@
 #'
 #' LoB_vals <- LoB(blank_df, col_lot = "reagent_lot", col_sample = "sample", col_value = "pg.ml")
 #'
-#' @return Returns the limit of blank (LoB) value(s).
 #'
 #' @export
 
 LoB <- function(df, col_lot, col_sample, col_value,
-                alpha=0.05, parametric = FALSE, always_sep_lots = FALSE){
-
-  # figure out how to use match.arg to allow only TRUE and FALSE for parametric and always_sep_lots
+                parametric = FALSE, alpha = 0.05, always_sep_lots = FALSE){
 
   # confirm that all col_sample and col_value exist
-  stopifnot("`col_lot` is not a column in df" = col_lot %in% names(df) | is.null(col_lot))
+  stopifnot("`col_lot` is not a column in df" = col_lot %in% names(df))
   stopifnot("`col_sample` is not a column in df" = col_sample %in% names(df))
   stopifnot("`col_value` is not a column in df" = col_value %in% names(df))
 
   # confirm that col_value is numeric
   stopifnot("`col_value` must be numeric" = is.numeric(df[[col_value]]))
 
+  # check for missing data
+  if(!all(complete.cases(df))){
+    # remove rows with missing values (and give warning)
+    df <- df[complete.cases(df),]
+    message("Warning: Ignoring rows with missing values.")
+  }
+
   # percentile
   pct <- 1 - alpha
 
-  # if col_lot is NULL, set n_lots equal to 1
-  if(is.null(col_lot)){
-    df$lot <- 1
-    n_lots <- 1
-  # otherwise set n_lots equal to # of unique values
-  }else{
-    df$lot <- df[[col_lot]]
-    n_lots <- unique(df$lot) |> length()
-  }
+  # find number of reagent lots
+  n_lots <- unique(df[[col_lot]]) |> length()
 
-  # if >3 lots and user has not set always_sep_lots = TRUE, combine all lots (CLSI guidelines)
-  if(!always_sep_lots & (n_lots > 3)){
-    df$lot <- 1
+  # if there are more than 3 lots and always_sep_lots = FALSE OR all reagent lot values are the same,
+  # reset all reagent lot values to 1
+  if((n_lots > 3 & !always_sep_lots) | n_lots == 1){
+    df[[col_lot]] <- 1
     n_lots <- 1
   }
 
-  if(always_sep_lots & n_lots >3){
+  if(always_sep_lots & n_lots > 3){
     message("Warning: Since there are at least four reagent lots in the data provided, CLSI guidelines recommend combining all reagent lots. Consider setting `always_sep_lots` = FALSE.")
   }
 
+  if(parametric){
+    # test to see if normally distributed
+    shapiro_pval <- stats::shapiro.test(df[[col_value]])$p.value |> round(4)
+    if(shapiro_pval <= 0.05){
+      message(paste0("Warning: These values do not appear to be normally distributed (Shapiro-Wilk test p-value = ", shapiro_pval,
+                     "). Consider the non-parametric approach (recommended)."))
+    }
+  }
+
   # loop through each reagent lot
-  LoB_val <- vector()
+  LoB_vals <- list()
   for(l in 1:n_lots){
 
     # look at each lot separately
-    lot_l <- df[df$lot == l,]
+    lot_l <- df[df[[col_lot]] == l,]
 
-    # number of reagents
+    # number of results
     B <- nrow(lot_l)
 
     # non-parametric
@@ -98,16 +108,11 @@ LoB <- function(df, col_lot, col_sample, col_value,
       sorted <- sort(lot_l[[col_value]])
 
       # interpolate measurement for exact rank position (LoB)
-      LoB_val[l] <- sorted[rank_below] + (rank_exact - rank_below)*(sorted[rank_above] - sorted[rank_below])
+      LoB_vals[l] <- sorted[rank_below] + (rank_exact - rank_below)*(sorted[rank_above] - sorted[rank_below])
     }
 
     # parametric
     if(parametric){
-
-      # test to see if normally distributed
-      if(stats::shapiro.test(lot_l[[col_value]])$p.value <= 0.05){
-        message("Warning: These values do not appear to be normally distributed. Consider a log transformation or the non-parametric approach.")
-      }
 
       # mean and SD
       mean_B <- mean(lot_l[[col_value]])
@@ -118,11 +123,16 @@ LoB <- function(df, col_lot, col_sample, col_value,
       cp <- stats::qnorm(pct) / (1 - (1/(4*(B-K)) ))
 
       # calculate LoB
-      LoB_val[l] <- mean_B + cp*sd_B
+      LoB_vals[l] <- mean_B + cp*sd_B
     }
-
-    # print LoB for each reagent lot (if separate)
-    print(paste0("LoB for lot ", l, ": ", LoB_val[l]))
+    names(LoB_vals)[l] <- paste0("LoB_lot_", l)
   }
-  return(LoB_val)
+
+  # if multiple LoB values, find the max
+  if(length(LoB_vals) > 1){
+    LoB_vals[n_lots + 1] <- unlist(LoB_vals) |> max()
+    names(LoB_vals)[n_lots + 1] <- "LoB_max"
+  }
+
+  return(LoB_vals)
 }
--- R/LoD.R
+++ R/LoD.R
@@ -1,50 +1,111 @@
 
 #' Limit of Detection (LoD) Calculation
 #'
-#' @param df data frame for one reagent lot
-#' @param col_values Name (in quotes) of the column with measurements.
-#' @param beta type II error
-#' @param LoB limit of blank
-#' @param approach method of LoD calculation
+#' @param df A data frame with low-level samples.
+#' @param col_lot Name (in quotes) of the column with reagent lot number.
+#' @param col_value Name (in quotes) of the column with measurements.
+#' @param LoB Limit of blank (LoB). This value should be the same for all reagent lots.
+#' @param approach Method of LoD calculation.
+#' @param beta Type II error. Default is 0.05.
+#' @param always_sep_lots If FALSE, reagent lots are evaluated according to CLSI guidelines
+#' (all lots evaluated separately if 2 or 3 lots, and all lots evaluated together if >3 lots).
+#' If TRUE, all reagent lots are evaluated separately regardless of the number of lots.
+#' Default is FALSE.
+#'
+#' @return Returns the limit of detection (LoD) value(s).
+#'
+#' @example
+#' reagent_lot <- c(rep(1, 12*5), rep(2, 12*5))
+#' day <- rep(rep(c(1, 2, 3), each = 4, times = 10))
+#' sample <- rep(c(1, 2, 3, 4, 5), each = 12, times = 2)
+#' replicate <- rep(c(1, 2, 3, 4), times = 3*5*2)
+#' pg.ml <- c(21.0, 22.8, 28.2, 25.9, 26.4, 28.3, 20.7, 21.9,
+#'             24.7, 22.5, 28.5, 29.2, 13.3, 12.6, 18.2, 14.7,
+#'             17.8, 14.0, 14.1, 12.5, 11.3, 12.2, 16.2, 13.9,
+#'             12.8, 12.9, 17.4, 16.0, 15.9, 14.1, 11.3, 9.4,
+#'             10.6, 13.6, 17.6, 14.9, 17.3, 19.2, 21.5, 22.2,
+#'             24.1, 25.8, 16.0, 16.4, 24.9, 23.8, 22.1, 26.1,
+#'             19.2, 22.7, 28.3, 26.2, 25.1, 30.3, 23.4, 19.2,
+#'             26.3, 23.1, 27.5, 30.1, 22.0, 22.5, 21.8, 22.1,
+#'             20.3, 21.0, 25.3, 26.0, 27.2, 25.1, 25.3, 25.3,
+#'             15.6, 21.2, 14.8, 14.9, 16.0, 15.8, 21.6, 22.8,
+#'             15.3, 18.7, 18.3, 19.5, 13.0, 15.9, 9.0, 7.0,
+#'             13.4, 8.5, 16.3, 18.1, 12.4, 11.1, 11.3, 10.1,
+#'             18.8, 17.6, 14.1, 14.9, 19.2, 15.8, 19.8, 21.4,
+#'             18.0, 18.0, 19.6, 23.1, 32.9, 30.4, 29.4, 27.6,
+#'             27.7, 30.6, 31.4, 30.4, 32.5, 28.9, 29.8, 35.1))
+#' lowlvl_df <- data.frame(reagent_lot, day, sample, replicate, pg.ml)
+#'
+#' LoD(lowlvl_df, col_lot = "reagent_lot", col_value = "pg.ml", LoB = 7.1, approach = "classical")
 #'
 #' @export
 
-# is L really supposed to be across all reagent lots?
+LoD <- function(df, col_lot, col_value, LoB,
+                approach = c("classical", "precision profile", "probit"),
+                beta = 0.05, always_sep_lots = FALSE){
 
-LoD <- function(df, col_values, beta=0.05, LoB,
-                approach = c("classical", "precision profile", "probit")){
+  # percentile
+  pct <- 1 - beta
 
-  # # percentile
-  # pct <- 1 - alpha
-  #
-  # # number of reagents
-  B <- nrow(df)
+  # classical (parametric)
+  if(approach == "classical"){
 
-  # classical nonparametric
-  if(approach == "classical parametric"){
+    # test to see if normally distributed
+    shapiro_pval <- shapiro.test(df[[col_value]])$p.value |> round(4)
+    if(shapiro_pval <= 0.05){
+      message(paste0("Warning: These values do not appear to be normally distributed (Shapiro-Wilk test p-value = ", shapiro_pval,
+                     "). Consider a mathematical transformation or a different approach."))
+    }
 
-    # maybe?
-    # df$pg.ml <- log(df$pg.ml)
+    # find number of reagent lots
+    n_lots <- unique(df[[col_lot]]) |> length()
 
-    # standard deviation for each sample
-    sd_sample_df <- do.call(data.frame,
-                             stats::aggregate(pg.ml ~ sample, data = df,
-                                       FUN = function(x) c(sd = stats::sd(x), n = length(x))))
+    # if there are more than 3 lots and always_sep_lots = FALSE OR all reagent lot values are the same,
+    # reset all reagent lot values to 1
+    if((n_lots > 3 & !always_sep_lots) | n_lots == 1){
+      df[[col_lot]] <- 1
+      n_lots <- 1
+    }
 
-    # pooled standard deviation
-    sd_L <- with(sd_sample_df, sqrt(((pg.ml.n - 1) %*% pg.ml.sd^2)/sum(pg.ml.n - 1)))
+    if(always_sep_lots & n_lots > 3){
+      message("Warning: Since there are at least four reagent lots in the data provided, CLSI guidelines recommend combining all reagent lots. Consider setting `always_sep_lots` = FALSE.")
+    }
 
-    # critical value
-    L <- B # double-check this! says across all reagent lots...
-    J <- unique(df$sample) |> length()
-    cp <- stats::qnorm(pct) / (1 - (1/(4*(L-J)) ))
+    # loop through each reagent lot
+    LoD_vals <- list()
+    for(l in 1:n_lots){
 
-    # calculate LoD
-    LoD_val <- LoB + cp*sd_L
+      # look at each lot separately
+      lot_l <- df[df[[col_lot]] == l,]
+
+      # standard deviation for each sample in each reagent lot
+      sd_sample_df <- do.call(data.frame, aggregate(pg.ml ~ sample, data = lot_l,
+                                                    FUN = function(x) c(sd = sd(x), n = length(x))))
+
+      # pooled standard deviation
+      sd_L <- with(sd_sample_df, sqrt( ((pg.ml.n-1) %*% pg.ml.sd^2) / sum(pg.ml.n-1)) )
+
+      # critical value
+      L <- nrow(lot_l) # number of results
+      J <- unique(lot_l$sample) |> length() # number of samples
+      cp <- qnorm(pct) / (1 - (1/(4*(L-J)) ))
+
+      # calculate LoD
+      LoD_vals[l] <- LoB + cp*sd_L
+      names(LoD_vals)[l] <- paste0("LoD_lot_", l)
+    }
+
   }
 
-  if(approach == "precision profile"){
-    LoD_val <- 0
+  # if(approach == "precision profile"){
+
+  # }
+
+  # if multiple LoD values, find the max
+  if(length(LoD_vals) > 1){
+    LoD_vals[n_lots + 1] <- unlist(LoD_vals) |> max()
+    names(LoD_vals)[n_lots + 1] <- "LoD_max"
   }
 
+  return(LoD_vals)
 }
